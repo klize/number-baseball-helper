a333e2bea42d8cd41f4c48253dfa403d480f421f
klize <klize31@gmail.com>
2020-08-31 22:27:59 +0900

    [wip] remove pyc, add case map

--- number_baseball_helper/case.py
+++ number_baseball_helper/case.py
@@ -4,4 +4,38 @@ class Case(tuple):
         return tuple.__new__(cls, (strike, ball))
 
 
+_case_to_name = {}
+_name_to_case = {}
 
+
+def make_cases(ndigit: int):
+    global _case_to_name
+    global _name_to_case
+
+    _case_to_name = _make_case_to_name(ndigit)
+    _name_to_case = _make_name_to_case(ndigit)
+
+
+def _make_case_to_name(ndigit: int):
+    cton = {}
+    for strike in range(ndigit):
+        possible_balls_cnt = ndigit - strike
+        if possible_balls_cnt == 1:
+            possible_balls = range(possible_balls_cnt)
+        else:
+            possible_balls = range(possible_balls_cnt + 1)
+
+        for ball in possible_balls:
+            case = Case(strike, ball, ndigit)
+            name = f"{strike} strike {ball} ball"
+
+            cton.update({case: name})
+    return cton
+
+
+def _make_name_to_case(ndigit: int):
+    pass
+
+
+def get_name(case: Case):
+   return _case_to_name.get(case)
--- number_baseball_helper/cmd/nbhelper/__init__.py
+++ number_baseball_helper/cmd/nbhelper/__init__.py
@@ -1,6 +1,52 @@
+from __future__ import print_function, unicode_literals
+
 import argparse
 import sys
+
+from PyInquirer import prompt
+
 from number_baseball_helper.helper import NumberBaseballHelper
+
+
+def console(app: NumberBaseballHelper):
+    _questions = [
+        {
+            "type": "list",
+            "name": "command",
+            "message": "Select job",
+            "choices": [
+                {
+                    "name": "Scan candidates",
+                    "value": app.scan_candidate.__name__
+                },
+                {
+                    "name": "Show candidates",
+                    "value": app.show_candidate.__name__
+                },
+                "Pick a random candidate",
+                {
+                    "name": "test",
+                    "value": 1
+                },
+                "Quit"
+            ]
+        },
+        {
+            "type": "list",
+            "name": "argument",
+            "message": "Of which case is your last guess ?",
+            "when": lambda answers: answers['command'] ==
+                                    app.scan_candidate.__name__
+            "choices": [
+                {
+                    "name": "3 Strike"
+                }
+            ]
+
+        }
+    ]
+    answers = prompt(_questions)
+    return answers
 
 
 def define_get_args(argv):
@@ -14,8 +60,8 @@ def main(argv=None):
     args = define_get_args(argv)
 
     helper = NumberBaseballHelper(ndigit=args.ndigit)
-    helper.start_console()
-
+    # helper.start_console()
+    print(answers)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
